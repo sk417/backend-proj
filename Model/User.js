ced6421e0d8828f10fda5e083f84534ce57da738
mihir.a <mihir.a@corp.zeta.tech>
2024-01-28 22:45:15 +0530

    plan file created

--- Model/User.js
+++ Model/User.js
@@ -2,6 +2,7 @@ const db = require('./DB');
 const {createUser, getAlluser} = require('./Query');
 
 const addUser = async (userData) => {
+    
     const {email, phone, firstName, lastName, gender, dob} = userData;
     const result = await db.query(createUser, [email, phone, firstName, lastName, gender, dob]);
     return result.rows[0];
