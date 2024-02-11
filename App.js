1224d2a00f467e2cc65b313af6623a98806df9c0
Pahal Jain <pahajain225@gmail.com>
2024-02-12 00:39:48 +0530

    adding essentials

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./Controller/Router/Route');
+const cors = require('cors')
 require("dotenv").config();   //.env is in root folder here
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
