975566cf137546431749788547dcff1def53e0d4
Sudheer Kumar <87257459+sk417@users.noreply.github.com>
2024-02-17 16:34:10 +0530

    minor changes

--- Model/Query.js
+++ Model/Query.js
@@ -3,23 +3,13 @@ const createUser = `INSERT INTO "mentors" (email, name, password, dob, designati
 
 const isExist = `SELECT * FROM "mentors" WHERE email = $1;`;
 
-const getAdminDashboardData = `SELECT id, name FROM "mentors";`;
+const getAdminDashboardData = `SELECT id, name, email, designation, dob FROM "mentors";`;
 
 const getMentorDashboardData = `SELECT mentees.id as id, mentees.name as name FROM "mentees" JOIN "mentors" ON mentees.mentor_id = mentors.id WHERE mentors.email = $1;`;
 
 const getPassword = `SELECT password FROM "mentors" WHERE email = $1;`;
 
 const getUserByEmail = `SELECT * FROM "mentors" WHERE email = $1;`;
-
-// const createComment = 'INSERT INTO mentees (name,week,comment,users_email,month) VALUES ($1,$2,$3,$4,$5) RETURNING *;' ;
-
-// const deleteComment = 'DELETE FROM  mentees WHERE id=$1 RETURNING *;' ;
-
-// const updateComment = 'UPDATE mentees SET comment=$1 WHERE id=$2 RETURNING *' ;
-
-// const getComment = 'SELECT name,week,id,comment FROM mentees WHERE users_email=$1 GROUP by (name,week,comment,id) ORDER BY name' ;
-
-// const checkDatequery = 'SELECT * FROM mentees WHERE (month =$1 AND week =$2  AND name=$3 AND users_email=$4)';
 
 const getCommentByMenteeId = `SELECT * FROM "comments" WHERE mentee_id = $1 AND week = $2;`;
 
